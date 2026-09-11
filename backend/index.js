330a12f005b2e273cc8378907fecef07ef337c9d
Yashpatil911 <yashjiu@gmail.com>
2026-09-11 19:08:02 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://virtualassistant-k01y.onrender.com",
   credentials: true
 }));
 let port = process.env.PORT || 8000;
