249b688a8368acf5c7f35d4fbeb73a88ed76c252
Yashpatil911 <yashjiu@gmail.com>
2026-09-11 18:58:04 +0530

    Change server URL to production
    Updated server URL to point to the production backend.

--- frontend/src/context/UserContxt.jsx
+++ frontend/src/context/UserContxt.jsx
@@ -3,7 +3,7 @@ export const UserDataContext = createContext();
 import axios from "axios";
 
 function UserContxt({ children }) {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://virtualassistant-backend-r25l.onrender.com";
   const [userData, setUserData] = useState(null);
   const [frontendImage, setFrontendImage] = useState(null);
   const [backendImage, setBackendImage] = useState(null);
